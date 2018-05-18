8fd2428541529cb2f0d4da157c134855a99d5bc5
chentao <1063191427@qq.com>
2018-05-18 17:33:22 +0800

    加密算法

---
+++ javautils-common/src/main/java/com/ju/common/AESUtil.java
@@ -0,0 +1,134 @@
+package com.ju.common;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @author zhi.li@91wutong.com
+ * @Description
+ * @created 2018/1/18 13:49
+ * @company 91wutong
+ */
+public class AESUtil {
+
+    /**
+     *  只能为16位
+     */
+    //public static final String KEY = "1234567891234567";
+    public static final String KEY = "1234567wqwqwwwww";
+    /**
+     * AES加密字符串
+     * @param input 原字符串
+     * @return
+     */
+    public static String encrypt(String input) {
+        byte[] crypted = null;
+        try {
+            SecretKeySpec skey = new SecretKeySpec(KEY.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, skey);
+            crypted = cipher.doFinal(input.getBytes());
+        } catch (Exception e) {
+        }
+        return new String(Base64.encodeBase64(crypted));
+    }
+
+    /**
+     * AES解密字符串
+     * @param input 加密的字符串
+     * @return
+     */
+    public static String decrypt(String input) {
+        byte[] output = null;
+        try {
+            SecretKeySpec skey = new SecretKeySpec(KEY.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, skey);
+            output = cipher.doFinal(Base64.decodeBase64(input.getBytes()));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return new String(output);
+    }
+
+    /**
+     * AES加密字符串
+     * @param input 原字符串
+     * @param key 加密的key
+     * @return
+     */
+    public static String encrypt(String input, String key) {
+        byte[] crypted = null;
+        try {
+            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, skey);
+            crypted = cipher.doFinal(input.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //return new String(Base64.encodeBase64(crypted));
+        return new String(crypted);
+    }
+
+    /**
+     * AES解密字符串
+     * @param input 加密的字符串
+     * @param key 加密的key
+     * @return
+     */
+    public static String decrypt(String input, String key) {
+        byte[] output = null;
+        try {
+            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, skey);
+            output = cipher.doFinal(Base64.decodeBase64(input.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new String(output);
+    }
+
+    public static String encryptHex(String input, String key) {
+        byte[] crypted = null;
+        try {
+            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, skey);
+            crypted = cipher.doFinal(input.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new String(Hex.encodeHexString(crypted));
+    }
+
+    public static String decryptHex(String input, String key) {
+        byte[] output = null;
+        try {
+            SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, skey);
+            output = cipher.doFinal(Hex.decodeHex(input.toCharArray()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new String(output);
+    }
+
+    public static void main(String[] args) {
+        String data = "168391";
+        System.out.println("加密前："+data);
+        System.out.println("加密后："+AESUtil.encrypt(data, KEY));
+        System.out.println("解密后："+AESUtil.decrypt(AESUtil.encrypt(data, KEY), KEY));
+        System.out.println("1231231"+data.equals(AESUtil.decrypt(AESUtil.encrypt(data, KEY), KEY)));
+
+
+       //System.out.println("解密后1111："+AESUtil.decrypt("K5hKpYj9H8rUX2UD22zWtg==", KEY));
+        /*System.out.println(AESUtil.encryptHex(data, KEY));
+        System.out.println(data.equals(AESUtil.decryptHex(AESUtil.encryptHex(data, KEY), KEY)));*/
+    }
+}
