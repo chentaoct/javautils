7e8bf5a7f1bc9f1b3b5c214b5c7d9765da1d1b32
chentao <1063191427@qq.com>
2018-05-22 11:52:35 +0800

    添加日志打印

--- javautils-web/src/main/java/com/ju/web/controller/exception/ExceptionController.java
+++ javautils-web/src/main/java/com/ju/web/controller/exception/ExceptionController.java
@@ -3,6 +3,7 @@ package com.ju.web.controller.exception;
 import com.ju.biz.manager.ExceptionManager;
 import com.ju.common.exception.BusinessException;
 import com.ju.model.base.BaseOneRespDTO;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("exceptionTest")
 public class ExceptionController {
+    private static final Logger logger = Logger.getLogger(ExceptionController.class);
     @Autowired
     private ExceptionManager exceptionManager;
 
@@ -25,6 +27,7 @@ public class ExceptionController {
         try {
             String resStr = exceptionManager.exceptionTest(name);
         } catch (BusinessException e) {
+            logger.error(e);
             res.setErrCode(e.getCode());
             res.setErrMsg(e.getMessage());
         } catch (Exception e) {
